9482f84b71beb7eb1090963d2efd13b89d8bbc8c
Vinay <rmdort@gmail.com>
2020-08-15 12:49:53 +0800

    Remove unused npm modules

--- packages/spreadsheet/.storybook/preview.js
+++ packages/spreadsheet/.storybook/preview.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
 import { theme, ThemeProvider } from "@chakra-ui/core";
 
-export const ThemeDecorator = props => {
+export const ThemeDecorator = (props) => {
   return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
 };
 
-addDecorator(storyFn => <ThemeDecorator>{storyFn()}</ThemeDecorator>);
+addDecorator((storyFn) => <ThemeDecorator>{storyFn()}</ThemeDecorator>);
